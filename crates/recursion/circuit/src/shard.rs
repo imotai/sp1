13bb19d9a06a2b9d2fc8da105f1ff2a9f4c3f38d
tamirhemo <tamirhmo@gmail.com>
2025-04-04 19:25:42 +0000

    chore: update test artifacts

--- crates/recursion/circuit/src/shard.rs
+++ crates/recursion/circuit/src/shard.rs
@@ -412,7 +412,7 @@ mod tests {
     async fn test_verify_shard() {
         setup_logger();
         let log_blowup = 1;
-        let log_stacking_height = 22;
+        let log_stacking_height = 21;
         let max_log_row_count = 22;
         let machine = RiscvAir::machine();
         let verifier = ShardVerifier::from_basefold_parameters(
