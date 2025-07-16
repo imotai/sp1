8366bbd82d4d1b93fc0a5e3f5d70951eba6bf3e8
rkm0959 <rkm0959@gmail.com>
2025-07-16 08:27:13 +0000

    fix: shape heights should be multiple of 32

--- crates/prover/src/shapes.rs
+++ crates/prover/src/shapes.rs
@@ -176,10 +176,10 @@ impl SP1RecursionProofShape {
                 (CompressAir::<BabyBear>::MemoryConst(MemoryConstChip::default()), 347_424),
                 (CompressAir::<BabyBear>::MemoryVar(MemoryVarChip::default()), 442_208),
                 (CompressAir::<BabyBear>::BaseAlu(BaseAluChip), 413_504),
-                (CompressAir::<BabyBear>::ExtAlu(ExtAluChip), 761_325),
+                (CompressAir::<BabyBear>::ExtAlu(ExtAluChip), 761_344),
                 (CompressAir::<BabyBear>::Poseidon2Wide(Poseidon2WideChip), 101_536),
-                (CompressAir::<BabyBear>::PrefixSumChecks(PrefixSumChecksChip), 239_880),
-                (CompressAir::<BabyBear>::Select(SelectChip), 700_008),
+                (CompressAir::<BabyBear>::PrefixSumChecks(PrefixSumChecksChip), 239_904),
+                (CompressAir::<BabyBear>::Select(SelectChip), 700_032),
                 (CompressAir::<BabyBear>::PublicValues(PublicValuesChip), 16),
             ]
             .into_iter()
