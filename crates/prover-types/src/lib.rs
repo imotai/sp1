8f6170d032ce7d0feb72f30cf7723626bcb48cd9
Yuwen Zhang <7747842+yuwen01@users.noreply.github.com>
2025-10-22 13:16:05 -0700

    chore: add convenience method (#900)
    * wip: refactor prover for proto types
    
    * remove old taskkind
    
    * finished refactor
    
    * old interface
    
    * cleanup
    
    * proto in setup
    
    * install with action
    
    * tonic include proto
    
    * convencience methods

--- crates/prover-types/src/lib.rs
+++ crates/prover-types/src/lib.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Result};
+
 mod artifacts;
 pub use artifacts::*;
 
@@ -12,3 +14,23 @@ pub mod worker {
 pub use worker::*;
 
 mod utils;
+
+impl WorkerType {
+    pub fn from_task_type(task_type: TaskType) -> Self {
+        match task_type {
+            TaskType::Controller | TaskType::PlonkWrap | TaskType::Groth16Wrap => WorkerType::Cpu,
+            TaskType::ProveShard
+            | TaskType::RecursionReduce
+            | TaskType::RecursionDeferred
+            | TaskType::ShrinkWrap
+            | TaskType::SetupVkey => WorkerType::Gpu,
+            TaskType::MarkerDeferredRecord | TaskType::UnspecifiedTaskType => WorkerType::None,
+        }
+    }
+}
+
+impl WorkerTask {
+    pub fn data(&self) -> Result<&TaskData> {
+        self.data.as_ref().ok_or(anyhow!("no task data"))
+    }
+}
