c124824791268175232f3d0fc885f096aed4ec9b
Eugene Rabinovich <eugene@succinct.xyz>
2025-12-01 16:43:12 -0500

    fix: remove unique decoding radius comment (#969)
    Co-authored-by: succinct <succinct>

--- crates/primitives/src/fri_params.rs
+++ crates/primitives/src/fri_params.rs
@@ -40,7 +40,6 @@ pub const SP1_TARGET_BITS_OF_SECURITY: usize = 100;
 pub const SP1_PROOF_OF_WORK_BITS: usize = 16;
 
 pub fn unique_decoding_queries(log_blowup: usize) -> usize {
-    // For unique decoding, we need to query at least half the symbols in the codeword.
     let rate = 1.0 / (1 << log_blowup) as f64;
     let half_rate_plus_half = 0.5 + (rate / 2.0);
     (-((SP1_TARGET_BITS_OF_SECURITY - SP1_PROOF_OF_WORK_BITS) as f64) / half_rate_plus_half.log2())
