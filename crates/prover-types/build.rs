da2be0d04096626ce8c9f645e9adf8d260fb289f
Chris T. <chris@succinct.xyz>
2025-12-16 13:22:31 -0800

    fix(prover-types): build.rs

--- crates/prover-types/build.rs
+++ crates/prover-types/build.rs
@@ -1,12 +1,18 @@
 use chrono::prelude::*;
-use std::env;
+use std::{env, path::Path};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Add /usr/include if it exists
+    let mut includes = vec!["proto/"];
+    if Path::new("/usr/include").exists() {
+        includes.push("/usr/include");
+    }
+
     // Generate protobuf code to OUT_DIR (default behavior)
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
-        .compile_protos(&["proto/worker.proto", "proto/cluster.proto"], &["proto/"])?;
+        .compile_protos(&["proto/worker.proto", "proto/cluster.proto"], &includes)?;
 
     println!("cargo:rerun-if-env-changed=BUILD_GIT_SHA");
 
