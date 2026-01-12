90eb2951c12c26a4d06823b5db220a84691f02ea
Tobias Bergkvist <tobias@bergkv.ist>
2026-01-12 17:54:21 +0100

    feat: Download cuslop-server from sp1 instead of sp1-wip (#1040)

--- crates/cuda/src/server.rs
+++ crates/cuda/src/server.rs
@@ -82,9 +82,7 @@ mod native {
             tracing::debug!("Downloading `cuslop-server`");
 
             let version = format!("v{}", sp1_primitives::SP1_VERSION);
-            let repo = "succinctlabs/sp1-wip";
-
-            // todo!(nhtyy): sp1-wip -> sp1
+            let repo = "succinctlabs/sp1";
             let static_url = format!("https://github.com/{repo}/releases/download");
             let asset_name = format!("cuslop_server_{version}_x86_64.tar.gz");
 
